cd64504d6e2b02d8f5c0f728bbd80eb3231ad275
Matthew Amaral <mamaral@lcs-mba3204.lcs.on.ca>
2017-10-02 10:33:57 -0400

    progress on the panda

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -23,11 +23,13 @@ import PlaygroundSupport
 // Create a new canvas
 let canvas = Canvas(width: 500, height: 500)
 
+canvas.translate(byX: 250, byY: 250)
+
 // create left ear
-canvas.drawEllipse(centreX: 100, centreY: 400, width: 100, height: 100, borderWidth: 1)
+canvas.drawEllipse(centreX: -150, centreY: 150, width: 100, height: 100, borderWidth: 1)
 
 // create right ear
-canvas.drawEllipse(centreX: 400, centreY: 400, width: 100, height: 100, borderWidth: 1)
+canvas.drawEllipse(centreX: 150, centreY: 150, width: 100, height: 100, borderWidth: 1)
 
 // change color to white
 canvas.fillColor = Color.white
@@ -36,17 +38,31 @@ canvas.fillColor = Color.white
 canvas.drawShapesWithBorders = false
 
 // create white face
-canvas.drawEllipse(centreX: 250, centreY: 250, width: 450, height:450, borderWidth: 1)
+canvas.drawEllipse(centreX: 0, centreY: 0, width: 420, height:420, borderWidth: 1)
 
 // change color to black
 canvas.fillColor = Color.black
 
-//rotate the canvas
-canvas.rotate(by: 5)
+//tilt the canvas
+canvas.rotate(by: -20)
 
 //create the left eye
-canvas.drawEllipse(centreX: 200, centreY: 300, width: 70, height:100, borderWidth: 1)
+canvas.drawEllipse(centreX: -80, centreY: 30, width: 70, height:100, borderWidth: 1)
 
+//tilt the canvas
+canvas.rotate(by: 40)
+
+//create the right eye
+canvas.drawEllipse(centreX: 90, centreY: 30, width: 70, height:100, borderWidth: 1)
+
+// change color to white
+canvas.fillColor = Color.white
+
+//creater the left white eye
+canvas.drawEllipse(centreX: -50, centreY: 100, width: 30, height: 30, borderWidth: 5)
+
+//creater the right white eye
+canvas.drawEllipse(centreX: 70, centreY: 60, width: 30, height: 30, borderWidth: 5)
 
 // This code is necessary to see the result in the Assistant Editor at right
 PlaygroundPage.current.liveView = canvas.imageView
